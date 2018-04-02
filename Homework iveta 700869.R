194c974a39113d2ea67a1e7f6752899eb5151312
Iveta Dimitrova <36793556+ivik09@users.noreply.github.com>
2018-04-02 16:31:49 +0300

    Add files via upload

---
+++ Homework iveta 700869.R
@@ -0,0 +1,27 @@
+#a
+x <- c(4, 1, 1, 4) 
+
+#b
+y <- c(1,4)
+
+#c
+sum(x)
+sum(y)
+
+#d
+s <- c(x,y)
+s
+
+#e
+s <- rep(s,10)
+length(s)
+
+#f
+rep(1:4, each = 3)
+
+#g
+seq(7, 21, by=1)
+7:21
+
+#h
+seq(length.out=5 )
